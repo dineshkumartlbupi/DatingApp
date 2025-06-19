dcd13e1dd0c716e47a07cef0cb616a5b76fa98ac
difmo <difmotech@gmail.com>
2025-06-19 11:54:49 +0530

    save

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -15,7 +15,7 @@ export default function TabLayout() {
         headerShown: false,
         tabBarStyle: Platform.select({
           ios: {
-            // Use a transparent background on iOS to show the blur effect
+            //Use a transparent background on iOS to show the blur effect
             position: "absolute",
           },
           default: {},
